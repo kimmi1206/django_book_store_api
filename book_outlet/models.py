7a54675124626bb337faae03efeadc2dc1c4ddff
kimmi1206 <kheinerimm1206@gmail.com>
2022-07-16 20:46:06 -0500

    created model relations

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,8 +1,68 @@
 from django.db import models
+# from django.contrib import admin
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.code})"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True, blank=True)
+
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
+
+    # @admin.display()
+    # def full_name(obj):
+    #     return ("%s %s" % (obj.first_name, obj.last_name))
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, blank=True, related_name="books")
+    is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, blank=True, db_index=True)
+    published_countries = models.ManyToManyField(Country)
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.rating})"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Avg
+# from django.http import Http404
+
+from .models import Book
 
 # Create your views here.
+
+
+def index(request):
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))  # returns a dictionary
+
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating,
+    })
+
+
+def book_detail(request, slug):
+    # try:
+    #     book = Book.objects.get(pk=id)  # book_id=id
+    # except Exception as exc:
+    #     raise Http404() from exc
+    book = get_object_or_404(Book, slug=slug)  # book_id=id
+
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
